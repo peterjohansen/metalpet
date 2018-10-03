8f244ce6d4a327847d79d6b223a7c54d3825e353
Peter André Johansen <peterandrejohansen@gmail.com>
2018-10-03 19:29:49 +0200

    Limit string modification handler parameter to correct type

--- metalpet-service/src/main/java/org/pemacy/metalpet/service/string/StringModificationHandler.java
+++ metalpet-service/src/main/java/org/pemacy/metalpet/service/string/StringModificationHandler.java
@@ -1,12 +1,14 @@
 package org.pemacy.metalpet.service.string;
 
+import org.pemacy.metalpet.model.string.StringModification;
+
 import java.util.function.BiFunction;
 
 /**
  * @author Peter André Johansen
  */
 @FunctionalInterface
-public interface StringModificationHandler<T> extends BiFunction<String, T, String> {
+public interface StringModificationHandler<T extends StringModification> extends BiFunction<String, T, String> {
 
 	@Override
 	String apply(String original, T modification);
