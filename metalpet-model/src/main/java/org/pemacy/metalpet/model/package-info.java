f957fa4c1939ffa21868ebd2d214d7d41391fb75
Peter André Johansen <peterandrejohansen@gmail.com>
2019-08-05 17:14:48 +0200

    Remove comment about a test

--- metalpet-model/src/main/java/org/pemacy/metalpet/model/package-info.java
+++ metalpet-model/src/main/java/org/pemacy/metalpet/model/package-info.java
@@ -1,7 +1,6 @@
 
 @Value.Style(
 	get = { "get*", "is*" },
-	// typeImmutable = "*", // No prefix or suffix for generated immutable type TODO Try this
 	validationMethod = ValidationMethod.VALIDATION_API
 )
 package org.pemacy.metalpet.model;
